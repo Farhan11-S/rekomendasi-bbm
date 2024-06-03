692aa5afe5c35a366f89ee33cb0c941eef286ce2
chirzarahman <chirzarhman@gmail.com>
2024-06-03 16:53:06 +0700

    feat: change logo and add joglo

--- form.php
+++ form.php
@@ -79,7 +79,8 @@ if (isset($_POST['kirim'])) {
     <link rel="stylesheet" href="https://taniku.kulonprogokab.go.id/assets/plugins/iCheck/square/blue.css">
 
     <!-- Google Font -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -105,11 +106,11 @@ if (isset($_POST['kirim'])) {
                         <div class='col-md-6'>
                             <?php if (isset($error)) : ?>
 
-                                <div class="error">
+                            <div class="error">
 
-                                    <?php echo $error ?>
+                                <?php echo $error ?>
 
-                                </div>
+                            </div>
 
                             <?php endif; ?>
                             <div class="form-group has-feedback" align="left">
@@ -119,34 +120,41 @@ if (isset($_POST['kirim'])) {
 
                             <div class="form-group has-feedback" align="left">
                                 <label>2) No ID Permohonan <p style="color: red; display: inline-block;">*</p></label>
-                                <input name="id_input" required type="text" class="form-control" placeholder="Masukkan Nomor ID Permohonan" value="<?php echo  $id ?>" disabled>
-                                <input name="id_input" required type="hidden" class="form-control" placeholder="Masukkan Nomor ID Permohonan" value="<?php echo $id ?>">
+                                <input name="id_input" required type="text" class="form-control"
+                                    placeholder="Masukkan Nomor ID Permohonan" value="<?php echo  $id ?>" disabled>
+                                <input name="id_input" required type="hidden" class="form-control"
+                                    placeholder="Masukkan Nomor ID Permohonan" value="<?php echo $id ?>">
                             </div>
 
                             <div class="form-group has-feedback" align="left">
                                 <label>3) NIK <p style="color: red; display: inline-block;">*</p></label>
-                                <input name="nik" required type="text" class="form-control" placeholder="NIK wajib diisi">
+                                <input name="nik" required type="text" class="form-control"
+                                    placeholder="NIK wajib diisi">
                             </div>
 
                             <div class="form-group has-feedback" align="left">
                                 <label>4) Nama Lengkap <p style="color: red; display: inline-block;">*</p></label>
-                                <input name="nama_lengkap" required type="text" class="form-control" placeholder="Masukkan Nama Lengkap">
+                                <input name="nama_lengkap" required type="text" class="form-control"
+                                    placeholder="Masukkan Nama Lengkap">
                             </div>
 
                             <div class="form-group has-feedback" align="left">
                                 <label>5) Alamat <p style="color: red; display: inline-block;">*</p></label>
-                                <input name="alamat" required type="text" class="form-control" placeholder="Masukkan Alamat">
+                                <input name="alamat" required type="text" class="form-control"
+                                    placeholder="Masukkan Alamat">
                             </div>
 
                             <div class="form-group has-feedback" align="left">
                                 <label>6) No. HP <p style="color: red; display: inline-block;">*</p></label>
-                                <input name="no_hp" required type="text" class="form-control" placeholder="Masukkan No. HP">
+                                <input name="no_hp" required type="text" class="form-control"
+                                    placeholder="Masukkan No. HP">
                             </div>
                         </div>
                         <div class='col-md-6'>
                             <div class="form-group" align="left">
                                 <label>7) Jenis BBM <p style="color: red; display: inline-block;">*</p></label>
-                                <select id="jenis_bbm" class="form-control" name="jenis_bbm" style="width: 45%;" required>
+                                <select id="jenis_bbm" class="form-control" name="jenis_bbm" style="width: 45%;"
+                                    required>
                                     <option value="">Pilih </option>
                                     <option value="Minyak Tanah">Minyak Tanah</option>
                                     <option value="Bensin (Gasoline) RON 88">Bensin (Gasoline) RON 88</option>
@@ -156,8 +164,10 @@ if (isset($_POST['kirim'])) {
 
                             </div>
                             <div class="form-group has-feedback" align="left">
-                                <label>8) Alokasi Volume BBM (liter / hari) <p style="color: red; display: inline-block;">*</p></label>
-                                <input name="volume_bbm_harian" type="text" class="form-control" placeholder="Tuliskan angka & huruf, contoh : 25 (dua puluh lima)" required>
+                                <label>8) Alokasi Volume BBM (liter / hari) <p
+                                        style="color: red; display: inline-block;">*</p></label>
+                                <input name="volume_bbm_harian" type="text" class="form-control"
+                                    placeholder="Tuliskan angka & huruf, contoh : 25 (dua puluh lima)" required>
                             </div>
                             <div style="margin: 20px 0px">
                                 <h4><b>9) BBM tersebut akan kami pergunakan untuk : </b> </h4>
@@ -165,8 +175,10 @@ if (isset($_POST['kirim'])) {
                             <div class='row'>
 
                                 <div align="left" class='form-group' style="margin-left: 17px;">
-                                    <label>a) Pilih Jenis Usaha <p style="color: red; display: inline-block;">*</p></label>
-                                    <select id="jenis_usaha" class="form-control" onchange="populate(this.value,'jenis_alat')" required>
+                                    <label>a) Pilih Jenis Usaha <p style="color: red; display: inline-block;">*</p>
+                                    </label>
+                                    <select id="jenis_usaha" class="form-control"
+                                        onchange="populate(this.value,'jenis_alat')" required>
                                         <option value=''>Pilih</option>
                                         <?php
                                         foreach ($jenisUsaha['jenis_usaha'] as $value) {
@@ -177,13 +189,16 @@ if (isset($_POST['kirim'])) {
                                 </div>
 
                                 <div align="left" class='form-group' style="margin-left: 17px;">
-                                    <label>b) Pilih Nama/Jenis Alat <p style="color: red; display: inline-block;">*</p></label>
-                                    <select name='jenis_usaha_id' class='form-control' id='jenis_alat' disabled required></select>
+                                    <label>b) Pilih Nama/Jenis Alat <p style="color: red; display: inline-block;">*</p>
+                                    </label>
+                                    <select name='jenis_usaha_id' class='form-control' id='jenis_alat' disabled
+                                        required></select>
                                 </div>
 
                                 <div align="left" class='form-group' style="margin-left: 17px;">
                                     <label>c) Nama Usaha <p style="color: red; display: inline-block;">*</p></label>
-                                    <input name="nama_usaha" type="text" class="form-control" placeholder="Isikan alat yg digunakan...." required>
+                                    <input name="nama_usaha" type="text" class="form-control"
+                                        placeholder="Isikan alat yg digunakan...." required>
 
                                 </div>
                             </div>
@@ -193,7 +208,8 @@ if (isset($_POST['kirim'])) {
                     <div class="row">
                         <div class="form-group has-feedback" align="left" style="margin-left: 17px;">
                             <label>10) Nomor Induk SPBU <p style="color: red; display: inline-block;">*</p></label>
-                            <input name="nomor_induk_spbu" type="text" class="form-control" placeholder="Isikan Nomor Induk SPBU" required>
+                            <input name="nomor_induk_spbu" type="text" class="form-control"
+                                placeholder="Isikan Nomor Induk SPBU" required>
                         </div>
 
                         <div class="form-group has-feedback" align="left" style="margin-left: 17px;">
@@ -203,21 +219,25 @@ if (isset($_POST['kirim'])) {
 
                         <div class="form-group has-feedback" align="left" style="margin-left: 17px;">
                             <label>12) Nomor Rangka Mesin <p style="color: red; display: inline-block;">*</p></label>
-                            <input name="nomor_rangka_mesin" type="text" class="form-control" placeholder="Isikan Nomor Rangka Mesin" required>
+                            <input name="nomor_rangka_mesin" type="text" class="form-control"
+                                placeholder="Isikan Nomor Rangka Mesin" required>
                         </div>
 
                         <div class="form-group has-feedback" align="left" style="margin-left: 17px;">
-                            <label class="form-label">13) Foto Mesin/Alat Penggerak <p style="color: red; display: inline-block;">*</p></label>
+                            <label class="form-label">13) Foto Mesin/Alat Penggerak <p
+                                    style="color: red; display: inline-block;">*</p></label>
                             <input type="file" class="form-control" name="foto_mesin" required />
                         </div>
 
                         <div class="form-group has-feedback" align="left" style="margin-left: 17px;">
-                            <label class="form-label">14) Foto KTP <p style="color: red; display: inline-block;">*</p></label>
+                            <label class="form-label">14) Foto KTP <p style="color: red; display: inline-block;">*</p>
+                            </label>
                             <input type="file" class="form-control" name="foto_ktp" required />
                         </div>
 
                         <div class="form-group has-feedback" align="left" style="margin-left: 17px;">
-                            <label class="form-label">15) Foto Keterangan Domisili Tempat Usaha dari Kepala Desa <p style="color: red; display: inline-block;">*</p></label>
+                            <label class="form-label">15) Foto Keterangan Domisili Tempat Usaha dari Kepala Desa <p
+                                    style="color: red; display: inline-block;">*</p></label>
                             <input type="file" class="form-control" name="foto_domisili" required />
                         </div>
                     </div>
@@ -247,7 +267,8 @@ if (isset($_POST['kirim'])) {
                         <div class="modal-dialog" role="document">
                             <div class="modal-content">
                                 <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                            aria-hidden="true">&times;</span></button>
                                     <h4 class="modal-title" id="myModalLabel">Simpan Pengajuan Surat Rekomendasi</h4>
                                 </div>
                                 <div class="modal-body">
@@ -272,31 +293,33 @@ if (isset($_POST['kirim'])) {
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js"
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+    </script>
 
     <script>
-        const jenisAlat = <?php echo json_encode($jenisUsaha['jenis_alat']) ?>
+    const jenisAlat = <?php echo json_encode($jenisUsaha['jenis_alat']) ?>
 
-        $.fn.isValid = function() {
-            return this[0].reportValidity()
+    $.fn.isValid = function() {
+        return this[0].reportValidity()
+    }
+
+    function populate(jenisUsaha) {
+        const select = document.getElementById('jenis_alat');
+
+        const tmp = jenisAlat
+            .filter((v) => v.jenis_usaha === jenisUsaha)
+            .map((v) => `<option value='${v.id}'>${v.jenis_alat}</option>`)
+
+        select.disabled = false
+        select.innerHTML = `<option value=''>Pilih</option>` + tmp.join()
+    }
+
+    function validateForm() {
+        if ($("#mainform").isValid()) {
+            $("#myModal").modal('show')
         }
-
-        function populate(jenisUsaha) {
-            const select = document.getElementById('jenis_alat');
-
-            const tmp = jenisAlat
-                .filter((v) => v.jenis_usaha === jenisUsaha)
-                .map((v) => `<option value='${v.id}'>${v.jenis_alat}</option>`)
-
-            select.disabled = false
-            select.innerHTML = `<option value=''>Pilih</option>` + tmp.join()
-        }
-
-        function validateForm() {
-            if ($("#mainform").isValid()) {
-                $("#myModal").modal('show')
-            }
-        }
+    }
     </script>
 </body>
 
--- login-admin.php
+++ login-admin.php
@@ -34,87 +34,95 @@ if (isset($_POST['kirim'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
     <title>Dinas Pertanian dan Pangan Kabupaten Kudus</title>
 </head>
 <style>
-    html,
-    body {
-        height: 100%;
-    }
+html,
+body {
+    height: 100%;
+}
 
-    body {
-        display: -ms-flexbox;
-        display: flex;
-        -ms-flex-align: center;
-        align-items: center;
-        padding-top: 40px;
-        padding-bottom: 40px;
-        background-color: #f5f5f5;
-    }
+body {
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-align: center;
+    align-items: center;
+    padding-top: 40px;
+    padding-bottom: 40px;
+    background-color: #f5f5f5;
+}
 
-    .form-signin {
-        width: 100%;
-        max-width: 330px;
-        padding: 15px;
-        margin: auto;
-    }
+.form-signin {
+    width: 100%;
+    max-width: 330px;
+    padding: 15px;
+    margin: auto;
+}
 
-    .form-signin .checkbox {
-        font-weight: 400;
-    }
+.form-signin .checkbox {
+    font-weight: 400;
+}
 
-    .form-signin .form-control {
-        position: relative;
-        box-sizing: border-box;
-        height: auto;
-        padding: 10px;
-        font-size: 16px;
-    }
+.form-signin .form-control {
+    position: relative;
+    box-sizing: border-box;
+    height: auto;
+    padding: 10px;
+    font-size: 16px;
+}
 
-    .form-signin .form-control:focus {
-        z-index: 2;
-    }
+.form-signin .form-control:focus {
+    z-index: 2;
+}
 
-    .form-signin input[type="text"] {
-        margin-bottom: -1px;
-        border-bottom-right-radius: 0;
-        border-bottom-left-radius: 0;
-    }
+.form-signin input[type="text"] {
+    margin-bottom: -1px;
+    border-bottom-right-radius: 0;
+    border-bottom-left-radius: 0;
+}
 
-    .form-signin input[type="password"] {
-        margin-bottom: 10px;
-        border-top-left-radius: 0;
-        border-top-right-radius: 0;
-    }
+.form-signin input[type="password"] {
+    margin-bottom: 10px;
+    border-top-left-radius: 0;
+    border-top-right-radius: 0;
+}
 </style>
 
 <body class="text-center">
     <form class="form-signin" method="post">
         <?php if (isset($error)) : ?>
 
-            <div class="error">
+        <div class="error">
 
-                <?php echo $error ?>
+            <?php echo $error ?>
 
-            </div>
+        </div>
 
         <?php endif; ?>
 
-        <img class="mb-4" src="https://getbootstrap.com/docs/4.1/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+        <img class="mb-4" src="assets/kab-kudus.png" alt="" width="120" height="120">
         <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
         <label for="inputUsername" class="sr-only">Username</label>
-        <input type="text" id="inputUsername" class="form-control" placeholder="Username" name="nama" required autofocus>
+        <input type="text" id="inputUsername" class="form-control" placeholder="Username" name="nama" required
+            autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" required>
         <button class="btn btn-lg btn-primary btn-block" type="submit" name="kirim">Sign in</button>
         <p class="mt-5 mb-3 text-muted">&copy; 2024-2025</p>
     </form>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
+        integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
